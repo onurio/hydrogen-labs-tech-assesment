6ae694df65be82412d4c01083f400a98b0af64e9
Omri Nuri <omrinuri@gmail.com>
2024-05-02 09:19:42 -0500

    initial commit

---
+++ BaseSFModule.ts
@@ -0,0 +1,100 @@
+import {
+  SFModule,
+  SFModuleConfig,
+  SFModuleNodeConfig,
+  SFModuleResponse,
+  defaultSFModuleConfig,
+} from "../types";
+
+export abstract class BaseSFModule implements SFModule {
+  public name: string;
+  public id: string;
+  public nodeConfig: SFModuleNodeConfig = defaultSFModuleConfig;
+  public config: SFModuleConfig | null = null;
+
+  constructor(name: string, id: string) {
+    this.name = name;
+    this.id = id;
+
+    this.querySafetyFactorConfig().then((config) => {
+      this.config = config;
+    });
+  }
+
+  private querySafetyFactorConfig(): Promise<SFModuleConfig> {
+    // return mock data
+    // Todo: replace with actual query
+    return Promise.resolve({
+      sfLowerBound: 0.2,
+      sfUpperBound: 0.3,
+    });
+  }
+
+  abstract queryProfitFromCorruption(): Promise<number>;
+  abstract queryCostOfCorruption(): Promise<number>;
+
+  private async safeyQueryProfitFromCorruption(): Promise<number | null> {
+    try {
+      return await this.queryProfitFromCorruption();
+    } catch (error) {
+      console.error(`Failed to query profit from corruption: ${error}`);
+      return null;
+    }
+  }
+
+  private async safeQueryCostOfCorruption(): Promise<number | null> {
+    try {
+      return await this.queryCostOfCorruption();
+    } catch (error) {
+      console.error(`Failed to query cost of corruption: ${error}`);
+      return null;
+    }
+  }
+
+  public async safeQuerySafetyFactorInfo(): Promise<SFModuleResponse> {
+    try {
+      let [PfC, CoC] = await Promise.all([
+        this.safeyQueryProfitFromCorruption(),
+        this.safeQueryCostOfCorruption(),
+      ]);
+
+      // Check if any of them is null or undefined, if they can be
+      if (PfC == null || CoC == null) {
+        return {
+          SF: null,
+          PfC,
+          CoC,
+          timestamp: Date.now(),
+          error: "PfC or CoC is null or undefined",
+        };
+      }
+
+      // Check for division by zero
+      if (CoC === 0) {
+        return {
+          SF: null,
+          PfC,
+          CoC,
+          timestamp: Date.now(),
+          error: "CoC is zero, cannot divide",
+        };
+      }
+
+      return {
+        SF: (CoC - PfC) / CoC,
+        PfC,
+        CoC,
+        timestamp: Date.now(),
+      };
+    } catch (error) {
+      console.error(`Failed to query safety factor info: ${error}`);
+      return {
+        SF: null,
+        PfC: null,
+        CoC: null,
+        timestamp: Date.now(),
+        error: (error as any).message || "An unknown error occurred",
+      };
+    }
+  }
+}
---
+++ MantleSFModule.ts
@@ -0,0 +1,13 @@
+import { BaseSFModule } from "./BaseSFModule";
+import viem from 'viem';
+
+export class MantleSFModule extends BaseSFModule {
+
+    async queryProfitFromCorruption(): Promise<number> {
+        // return mock data
+    }
+
+    async queryCostOfCorruption(): Promise<number> {
+        // return mock data
+    }
+}
