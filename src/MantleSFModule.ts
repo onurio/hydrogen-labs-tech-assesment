978d8d170b7b91d9c541c83bd7e2283c79345bb2
Omri Nuri <omrinuri@gmail.com>
2024-05-02 11:01:48 -0500

    general cleanup

--- src/MantleSFModule.ts
+++ src/MantleSFModule.ts
@@ -1,7 +1,6 @@
 
-import { Address, Chain, PublicClient, createPublicClient, getAddress, getContract, http } from "viem";
+import { Address, Chain, PublicClient, createPublicClient, formatUnits, getAddress, getContract, http } from "viem";
 import { BaseSFModule } from "./BaseSFModule";
-import viem from "viem";
 
 const mantleTestnet: Chain = {
     id: 5001, name: "Mantle Testnet", nativeCurrency: { decimals: 18, name: "Mantle", symbol: "MNT", },
@@ -40,17 +39,17 @@ export class MantleSFModule extends BaseSFModule {
 
 
     async queryProfitFromCorruption(): Promise<number> {
-        const balancePromises = contracts.map(async (contract) => {
-            const balance = await this.client.getBalance({ address: getAddress(contract.address) });
-            return parseFloat(balance.toString());
+        const balancePromises = contracts.map(async (contract): Promise<BigInt> => {
+            return await this.client.getBalance({ address: getAddress(contract.address) });
         });
         const balances = await Promise.all(balancePromises);
-        const totalPfC = balances.reduce((acc, balance) => acc + balance, 0);
-        return Promise.resolve(parseFloat(totalPfC.toString()));
+        // ts was complaining that I can't add BigInts. I'm not sure if this is the best way to do it
+        const totalPfC = balances.reduce((acc, balance) => acc + Number(balance), 0);
+        return Promise.resolve(totalPfC);
     }
 
     async queryCostOfCorruption(): Promise<number> {
-        // return mock data
+        // get the Mantle staking value from the contract
         return Promise.resolve(1000);
     }
 }
